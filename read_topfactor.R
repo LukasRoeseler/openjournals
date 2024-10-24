10dd7c871031b8f44a3c6041d4c92eb67c402919
LukasRoeseler <lukas.roeseler@uni-muenster.de>
2024-10-24 15:27:25 +0200

    Combined multiple plots by level

--- read_topfactor.R
+++ read_topfactor.R
@@ -1,5 +1,6 @@
 # Read TOPfactor.org Data -------------------------------------------------
 library(ggplot2)
+library(ggpubr)
 
 
 topfactor <- read.csv("https://osf.io/qatkz/download/")
@@ -47,9 +48,19 @@ topranks <- data.frame("top" = c("Data.citation.score"
                                  , "Registered.reports...publication.bias.score"
                                  , "Design...analysis.reporting.guidelines.score")
                        , "adoption" = colSums(topfactortable>0, na.rm = TRUE)/nrow(topfactortable)
+                       , "adoption2" = colSums(topfactortable>1, na.rm = TRUE)/nrow(topfactortable)
+                       , "adoption3" = colSums(topfactortable>2, na.rm = TRUE)/nrow(topfactortable)
 )
 
 topranks$top <- substr(topranks$top, 0, nchar(topranks$top)-6)
 
-ggplot(topranks, aes(x = adoption, y = reorder(top, adoption))) + geom_bar(stat = "identity", col = "black", fill = "grey") + xlab("Proportion of Journals with Score > 0") + ylab("TOP Criterion") +
+t1 <- ggplot(topranks, aes(x = adoption, y = reorder(top, adoption))) + geom_bar(stat = "identity", col = "black", fill = "grey") + xlab("Proportion of Journals with Score > 0") + ylab("TOP Criterion") +
   theme_bw() + xlim(c(0, 1))
+
+t2 <- ggplot(topranks, aes(x = adoption2, y = reorder(top, adoption))) + geom_bar(stat = "identity", col = "black", fill = "grey") + xlab("Proportion of Journals with Score > 1") + ylab("TOP Criterion") +
+  theme_bw() + xlim(c(0, 1))
+
+t3 <- ggplot(topranks, aes(x = adoption3, y = reorder(top, adoption))) + geom_bar(stat = "identity", col = "black", fill = "grey") + xlab("Proportion of Journals with Score > 2") + ylab("TOP Criterion") +
+  theme_bw() + xlim(c(0, 1))
+
+ggarrange(t1, t2, t3, nrow = 1, labels = c("Level 1 or higher", "Level 2 or higher", "Level 3"))
